103472dcb5de337c776faba5d157c11d7846153c
Zhuowei Chen <13602337187@163.com>
2024-09-22 00:11:16 +0800

    Update editor.py

--- easyeditor/editors/editor.py
+++ easyeditor/editors/editor.py
@@ -86,7 +86,7 @@ class BaseEditor:
                 self.tok.pad_token_id = self.tok.eos_token_id
             elif 'llama' in self.model_name.lower():
                 self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch_dtype, device_map=device_map,token="")
-                self.tok = AutoTokenizer.from_pretrained(self.model_name,token="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")
+                self.tok = AutoTokenizer.from_pretrained(self.model_name,token="")
                 self.tok.pad_token_id = self.tok.eos_token_id
             elif 'baichuan' in self.model_name.lower():
                 self.model = AutoModelForCausalLM.from_pretrained(self.model_name, torch_dtype=torch_dtype, trust_remote_code=True, device_map=device_map)
